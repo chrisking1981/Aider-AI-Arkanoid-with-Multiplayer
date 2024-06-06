10a28f7c33d1e5598ef0884bee8f8055cd43da36
chrisking1981 <48853558+chrisking1981@users.noreply.github.com>
2024-06-06 16:24:16 +0200

    Added handling for a new laser power-up in the game.

--- powerups.py
+++ powerups.py
@@ -6,16 +6,24 @@ SHIELD_WIDTH = 100
 SHIELD_HEIGHT = 10
 SHIELD_DROP_CHANCE = 0.3
 ENLARGE_DROP_CHANCE = 0.2
+LASER_DROP_CHANCE = 0.1
 
-def handle_powerups(bricks, paddle, shield, enlarge, shield_active, enlarge_active, shield_sound, enlarge_sound):
+def handle_powerups(bricks, paddle, shield, enlarge, laser, shield_active, enlarge_active, laser_active, shield_sound, enlarge_sound, laser_sound):
     for brick in bricks[:]:
         if random.random() < SHIELD_DROP_CHANCE:
             shield = pygame.Rect(brick.x + brick.width // 2 - SHIELD_WIDTH // 2, brick.y, SHIELD_WIDTH, SHIELD_HEIGHT)
         elif random.random() < ENLARGE_DROP_CHANCE:
             enlarge = pygame.Rect(brick.x + brick.width // 2 - SHIELD_WIDTH // 2, brick.y, SHIELD_WIDTH, SHIELD_HEIGHT)
-    return shield, enlarge
+    for brick in bricks[:]:
+        if random.random() < SHIELD_DROP_CHANCE:
+            shield = pygame.Rect(brick.x + brick.width // 2 - SHIELD_WIDTH // 2, brick.y, SHIELD_WIDTH, SHIELD_HEIGHT)
+        elif random.random() < ENLARGE_DROP_CHANCE:
+            enlarge = pygame.Rect(brick.x + brick.width // 2 - SHIELD_WIDTH // 2, brick.y, SHIELD_WIDTH, SHIELD_HEIGHT)
+        elif random.random() < LASER_DROP_CHANCE:
+            laser = pygame.Rect(brick.x + brick.width // 2 - SHIELD_WIDTH // 2, brick.y, SHIELD_WIDTH, SHIELD_HEIGHT)
+    return shield, enlarge, laser
 
-def update_powerups(shield, enlarge, paddle, shield_active, enlarge_active, shield_sound, enlarge_sound, countdown_start_time, SCREEN_HEIGHT):
+def update_powerups(shield, enlarge, laser, paddle, shield_active, enlarge_active, laser_active, shield_sound, enlarge_sound, laser_sound, countdown_start_time, SCREEN_HEIGHT):
     if shield:
         shield.y += 5
         if shield.colliderect(paddle):
@@ -35,4 +43,13 @@ def update_powerups(shield, enlarge, paddle, shield_active, enlarge_active, shie
         elif enlarge.y > SCREEN_HEIGHT:
             enlarge = None
 
-    return shield, enlarge, shield_active, enlarge_active, countdown_start_time
+    if laser:
+        laser.y += 5
+        if laser.colliderect(paddle):
+            laser_active = True
+            laser_sound.play()
+            laser = None
+        elif laser.y > SCREEN_HEIGHT:
+            laser = None
+
+    return shield, enlarge, laser, shield_active, enlarge_active, laser_active, countdown_start_time
