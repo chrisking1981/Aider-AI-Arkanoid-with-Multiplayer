127fe193ad4991eea452fb396d5c28f5dc4259aa
chrisking1981 <48853558+chrisking1981@users.noreply.github.com>
2024-06-08 12:06:43 +0200

    Refactored power-ups to be managed by a new PowerUpManager class.

--- powerups.py
+++ powerups.py
@@ -1,160 +1,97 @@
 import pygame
 import random
 
-# Power-ups
-SHIELD_WIDTH = 20
-SHIELD_HEIGHT = 20
-SHIELD_SIZE = 20
-LASER_SIZE = 20
-LASER_DROP_CHANCE = 0.1
-STICKY_SIZE = 20
-STICKY_DROP_CHANCE = 0.15
+class PowerUpManager:
+     SHIELD_WIDTH = 20
+     SHIELD_HEIGHT = 20
+     SHIELD_SIZE = 20
+     LASER_SIZE = 20
+     LASER_DROP_CHANCE = 0.1
+     STICKY_SIZE = 20
+     STICKY_DROP_CHANCE = 0.15
 
-def handle_powerups(brick, paddle, shield, enlarge, laser, sticky, shield_active, enlarge_active, laser_active, sticky_active, shield_sound, enlarge_sound, laser_sound, sticky_sound):
-    if not (shield or enlarge or laser or sticky):
-        powerups = ['shield', 'enlarge', 'laser', 'sticky']
-        selected_powerup = random.choice(powerups)
-        if selected_powerup == 'shield':
-            shield = pygame.Rect(brick.x + brick.width // 2 - SHIELD_SIZE // 2, brick.y, SHIELD_SIZE, SHIELD_SIZE)
-        elif selected_powerup == 'enlarge':
-            enlarge = pygame.Rect(brick.x + brick.width // 2 - SHIELD_SIZE // 2, brick.y, SHIELD_SIZE, SHIELD_SIZE)
-        elif selected_powerup == 'laser':
-            laser = pygame.Rect(brick.x + brick.width // 2 - LASER_SIZE // 2, brick.y, LASER_SIZE, LASER_SIZE)
-    return shield, enlarge, laser, sticky
+     def __init__(self):
+         self.shield = None
+         self.enlarge = None
+         self.laser = None
+         self.sticky = None
 
-def update_powerups(shield, enlarge, laser, sticky, paddle, shield_active, enlarge_active, laser_active, sticky_active, shield_sound, enlarge_sound, laser_sound, sticky_sound, countdown_start_time, SCREEN_HEIGHT):
-    if shield:
-        shield.y += 5
-        if shield.colliderect(paddle):
-            shield_active = True
-            shield_sound.play()
-            shield = None
-            countdown_start_time = pygame.time.get_ticks()
-        elif shield.y > SCREEN_HEIGHT:
-            shield = None
+     def handle_powerups(self, brick, paddle, game):
+         if not (self.shield or self.enlarge or self.laser or self.sticky):
+             powerups = ['shield', 'enlarge', 'laser', 'sticky']
+             selected_powerup = random.choice(powerups)
+             if selected_powerup == 'shield':
+                 self.shield = pygame.Rect(brick.rect.x + brick.rect.width // 2 - self.SHIELD_SIZE // 2, brick.rect.y, self.SHIELD_SIZE, self.SHIELD_SIZE)
+             elif selected_powerup == 'enlarge':
+                 self.enlarge = pygame.Rect(brick.rect.x + brick.rect.width // 2 - self.SHIELD_SIZE // 2, brick.rect.y, self.SHIELD_SIZE, self.SHIELD_SIZE)
+             elif selected_powerup == 'laser':
+                 self.laser = pygame.Rect(brick.rect.x + brick.rect.width // 2 - self.LASER_SIZE // 2, brick.rect.y, self.LASER_SIZE, self.LASER_SIZE)
+         return self.shield, self.enlarge, self.laser, self.sticky
 
-    if enlarge:
-        enlarge.y += 5
-        if enlarge.colliderect(paddle):
-            enlarge_active = True
-            enlarge_sound.play()
-            enlarge = None
-        elif enlarge.y > SCREEN_HEIGHT:
-            enlarge = None
+     def update(self, paddle, game):
+         self.update_shield(paddle, game)
+         self.update_enlarge(paddle, game)
+         self.update_laser(paddle, game)
+         self.update_sticky(paddle, game)
 
-    if laser:
-        laser.y += 5
-        if laser.colliderect(paddle):
-            laser_active = True
-            laser_sound.play()
-            laser = None
-        elif laser.y > SCREEN_HEIGHT:
-            laser = None
+     def update_shield(self, paddle, game):
+         if self.shield:
+             self.shield.y += 5
+             if self.shield.colliderect(paddle.rect):
+                 game.shield_active = True
+                 game.shield_sound.play()
+                 self.shield = None
+                 game.countdown_start_time = pygame.time.get_ticks()
+             elif self.shield.y > game.screen_height:
+                 self.shield = None
 
-    if sticky:
-        sticky.y += 5
-        if sticky.colliderect(paddle):
-            sticky_active = True
-            sticky_sound.play()
-            sticky = None
-        elif sticky.y > SCREEN_HEIGHT:
-            sticky = None
+     def update_enlarge(self, paddle, game):
+         if self.enlarge:
+             self.enlarge.y += 5
+             if self.enlarge.colliderect(paddle.rect):
+                 game.enlarge_active = True
+                 game.enlarge_sound.play()
+                 self.enlarge = None
+             elif self.enlarge.y > game.screen_height:
+                 self.enlarge = None
 
-    return shield, enlarge, laser, sticky, shield_active, enlarge_active, laser_active, sticky_active, countdown_start_time
+     def update_laser(self, paddle, game):
+         if self.laser:
+             self.laser.y += 5
+             if self.laser.colliderect(paddle.rect):
+                 game.laser_active = True
+                 game.laser_sound.play()
+                 self.laser = None
+             elif self.laser.y > game.screen_height:
+                 self.laser = None
 
-def draw_powerups(screen, font, shield, enlarge, laser, sticky):
-    if shield:
-        pygame.draw.rect(screen, GREEN, shield)
-        text = font.render("S", True, WHITE)
-        screen.blit(text, (shield.x + 5, shield.y + 5))
+     def update_sticky(self, paddle, game):
+         if self.sticky:
+             self.sticky.y += 5
+             if self.sticky.colliderect(paddle.rect):
+                 game.sticky_active = True
+                 game.sticky_sound.play()
+                 self.sticky = None
+             elif self.sticky.y > game.screen_height:
+                 self.sticky = None
 
-    if enlarge:
-        pygame.draw.rect(screen, BLUE, enlarge)
-        text = font.render("E", True, WHITE)
-        screen.blit(text, (enlarge.x + 5, enlarge.y + 5))
+     def draw(self, screen, font):
+         if self.shield:
+             pygame.draw.rect(screen, (0, 255, 0), self.shield)
+             text = font.render("S", True, (255, 255, 255))
+             screen.blit(text, (self.shield.x + 5, self.shield.y + 5))
 
-    if laser:
-        pygame.draw.rect(screen, RED, laser)
-        text = font.render("L", True, WHITE)
-        screen.blit(text, (laser.x + 5, laser.y + 5))
+         if self.enlarge:
+             pygame.draw.rect(screen, (0, 0, 255), self.enlarge)
+             text = font.render("E", True, (255, 255, 255))
+             screen.blit(text, (self.enlarge.x + 5, self.enlarge.y + 5))
 
-# Shield constants
-SHIELD_WIDTH = 20
-SHIELD_HEIGHT = 20
-SHIELD_SIZE = 20
-SHIELD_DROP_CHANCE = 0.3
+         if self.laser:
+             pygame.draw.rect(screen, (255, 0, 0), self.laser)
+             text = font.render("L", True, (255, 255, 255))
+             screen.blit(text, (self.laser.x + 5, self.laser.y + 5))
 
-def handle_shield(brick, shield):
-    if not shield:
-        drop_chance = random.random()
-        if drop_chance < SHIELD_DROP_CHANCE:
-            shield = pygame.Rect(brick.x + brick.width // 2 - SHIELD_SIZE // 2, brick.y, SHIELD_SIZE, SHIELD_SIZE)
-    return shield
-
-def update_shield(shield, paddle, shield_active, shield_sound, countdown_start_time, SCREEN_HEIGHT):
-    if shield:
-        shield.y += 5
-        if shield.colliderect(paddle):
-            shield_active = True
-            shield_sound.play()
-            shield = None
-            countdown_start_time = pygame.time.get_ticks()
-        elif shield.y > SCREEN_HEIGHT:
-            shield = None
-    return shield, shield_active, countdown_start_time
-# Laser constants
-LASER_COOLDOWN = 125  # Cooldown period in milliseconds
-
-def shoot_laser(paddle, lasers, last_shot_time, cooldown, laser_sound):
-    current_time = pygame.time.get_ticks()
-    if current_time - last_shot_time >= cooldown:
-        laser = pygame.Rect(paddle.x + paddle.width // 2 - 2, paddle.y - 20, 4, 20)
-        pygame.draw.rect(pygame.display.get_surface(), (255, 0, 0), laser)  # Draw the laser in red
-        lasers.append(laser)
-        laser_sound.play()
-        return current_time
-    return last_shot_time
-
-def update_lasers(lasers, bricks, brick_hit_sound, paddle, shield, enlarge, laser, sticky, shield_active, enlarge_active, laser_active, sticky_active, shield_sound, enlarge_sound, laser_sound, sticky_sound):
-    for laser in lasers[:]:
-        laser.y -= 15
-        if laser.y < 0:
-            lasers.remove(laser)
-        else:
-            for brick in bricks[:]:
-                if laser.colliderect(brick):
-                    bricks.remove(brick)
-                    lasers.remove(laser)
-                    brick_hit_sound.play()
-                    shield, enlarge, laser = handle_powerups(brick, paddle, shield, enlarge, laser, shield_active, enlarge_active, laser_active, shield_sound, enlarge_sound, laser_sound)
-                    break
-    return lasers, shield, enlarge, laser, sticky
-    return lasers
-# Enlarge constants
-ENLARGE_DROP_CHANCE = 0.2
-
-def handle_enlarge(brick, enlarge):
-    if not enlarge:
-        drop_chance = random.random()
-        if drop_chance < ENLARGE_DROP_CHANCE:
-            enlarge = pygame.Rect(brick.x + brick.width // 2 - SHIELD_SIZE // 2, brick.y, SHIELD_SIZE, SHIELD_SIZE)
-    return enlarge
-
-def update_enlarge(enlarge, paddle, enlarge_active, enlarge_sound, SCREEN_HEIGHT):
-    if enlarge:
-        enlarge.y += 5
-        if enlarge.colliderect(paddle):
-            enlarge_active = True
-            enlarge_sound.play()
-            enlarge = None
-        elif enlarge.y > SCREEN_HEIGHT:
-            enlarge = None
-    return enlarge, enlarge_active
-    if sticky:
-        pygame.draw.rect(screen, YELLOW, sticky)
-        text = font.render("T", True, WHITE)
-        screen.blit(text, (sticky.x + 5, sticky.y + 5))
-    if sticky:
-        pygame.draw.rect(screen, YELLOW, sticky)
-        text = font.render("T", True, WHITE)
-        screen.blit(text, (sticky.x + 5, sticky.y + 5))
+         if self.sticky:
+             pygame.draw.rect(screen, (255, 255, 0), self.sticky)
+             text = font.render("T", True, (255, 255, 255))
+             screen.blit(text, (self.sticky.x + 5, self.sticky.y + 5))
