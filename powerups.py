5887327247c6b4ff0ff14be850a170a3e83e1cb4
chrisking1981 <48853558+chrisking1981@users.noreply.github.com>
2024-06-08 12:20:11 +0200

    Added new attributes and methods to the Laser class.

--- powerups.py
+++ powerups.py
@@ -108,3 +108,13 @@ class Laser:
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), self.rect)
+class Laser:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 5, 20)
+        self.speed = 10
+
+    def move(self):
+        self.rect.y -= self.speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), self.rect)
