beb4a78b2d53f6754832b2d4f305d7c15a25e99f
chrisking1981 <48853558+chrisking1981@users.noreply.github.com>
2024-06-08 12:01:07 +0200

    Refactored brick module to use a class for creating and drawing bricks.

--- brick.py
+++ brick.py
@@ -1,22 +1,25 @@
-import pygame
-from colors import BLUE
+ import pygame
+ from colors import BLUE
 
-BRICK_WIDTH = 75
-BRICK_HEIGHT = 20
-BRICK_ROWS = 5
-BRICK_COLUMNS = 10
+ BRICK_WIDTH = 75
+ BRICK_HEIGHT = 20
+ BRICK_ROWS = 5
+ BRICK_COLUMNS = 10
 
-def create_brick(x, y):
-    return pygame.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
+ class Brick:
+     def __init__(self, x, y):
+         self.rect = pygame.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
 
-def draw_brick(screen, brick, color, scale_x, scale_y):
-    scaled_brick = pygame.Rect(brick.x * scale_x, brick.y * scale_y, brick.width * scale_x, brick.height * scale_y)
-    pygame.draw.rect(screen, color, scaled_brick)
+     @staticmethod
+     def create_bricks():
+         bricks = []
+         for row in range(BRICK_ROWS):
+             for col in range(BRICK_COLUMNS):
+                 brick = Brick(col * (BRICK_WIDTH + 10) + 35, row * (BRICK_HEIGHT + 10) + 35)
+                 bricks.append(brick)
+         return bricks
 
-def create_bricks():
-    bricks = []
-    for row in range(BRICK_ROWS):
-        for col in range(BRICK_COLUMNS):
-            brick = create_brick(col * (BRICK_WIDTH + 10) + 35, row * (BRICK_HEIGHT + 10) + 35)
-            bricks.append(brick)
-    return bricks
+     @staticmethod
+     def draw(screen, brick, color, scale_x, scale_y):
+         scaled_brick = pygame.Rect(brick.rect.x * scale_x, brick.rect.y * scale_y, brick.rect.width * scale_x, brick.rect.height * scale_y)
+         pygame.draw.rect(screen, color, scaled_brick)
